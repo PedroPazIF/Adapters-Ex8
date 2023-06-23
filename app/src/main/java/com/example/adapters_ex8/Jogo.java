1b969d2bbc2f168855d282238e599fdbc4ce8f9b
PedroPazIF <pedropazif@gmail.com>
2023-06-22 23:38:26 -0300

    Finalizado

--- app/src/main/java/com/example/adapters_ex8/Jogo.java
+++ app/src/main/java/com/example/adapters_ex8/Jogo.java
@@ -19,11 +19,11 @@ public class Jogo {
         jogos.add(new Jogo("Valorant","2 de junho de 2020",R.drawable.valorant));
         jogos.add(new Jogo("TFT","26 de junho de 2019",R.drawable.tft));
         jogos.add(new Jogo("CounterStrike","21 de agosto de 2012",R.drawable.cs));
-        jogos.add(new Jogo("Nba2k22",R.drawable.nba));
-        jogos.add(new Jogo("Limbo",R.drawable.limbo));
-        jogos.add(new Jogo("Spore",R.drawable.spore));
-        jogos.add(new Jogo("Ori and the blind forest",R.drawable.ori));
-        jogos.add(new Jogo("The sims 4",R.drawable.thesims));
+        jogos.add(new Jogo("Nba2k22","9 de setembro de 2021",R.drawable.nba));
+        jogos.add(new Jogo("Limbo","21 de julho de 2010",R.drawable.limbo));
+        jogos.add(new Jogo("Spore","4 de setembro de 2008",R.drawable.spore));
+        jogos.add(new Jogo("Ori and the blind forest","11 de março de 2015",R.drawable.ori));
+        jogos.add(new Jogo("The sims 4","2 de setembro de 2014",R.drawable.thesims));
         return jogos;
     }
 }
